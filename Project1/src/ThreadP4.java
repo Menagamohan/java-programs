2ef541a6e9cfc53b9e070fbe61e0d2cd3af4ee92
Menaga Mohanraj <menagamohan26@gmail.com>
2024-12-15 11:10:59 +0530

    Add files via upload

---
+++ Project1/src/ThreadP4.java
@@ -0,0 +1,18 @@
+
+public class ThreadP4 extends Thread 
+{
+	public void run()
+	{
+		System.out.println("CurrentThread Name is::"+getName());
+		System.out.println("CurrentThread Priority::"+getPriority());
+	}
+
+	public static void main(String[] args) {
+		ThreadP4 obj1=new ThreadP4();
+		obj1.start();
+		obj1.setPriority(MAX_PRIORITY);
+		obj1.setName("MK");
+
+	}
+
+}
